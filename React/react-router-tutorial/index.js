ccc0a13994d896759fc1cb4a0769a3c6bcd1f340
VitorOta <ota.vitor@gmail.com>
2017-10-10 00:41:06 -0300

    react router tutorial

---
+++ React/react-router-tutorial/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './modules/App'
+import About from './modules/About'
+import Repos from './modules/Repos'
+import Repo from './modules/Repo'
+import Home from './modules/Home'
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+
+render(
+    (
+        <Router history={hashHistory}>
+            <Route path="/" component={App}>
+                <IndexRoute component={Home} />
+                <Route path="/repos" component={Repos}>
+                <Route path="/repos/:userName/:repoName" component={Repo} />
+                </Route>
+                <Route path="/about" component={About} />
+            </Route>
+        </Router>
+    )
+    , document.getElementById('app'))
---
+++ React/react-router-tutorial/modules/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { IndexLink } from 'react-router'
+import NavLink from './NavLink'
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>React Router Tutorial</h1>
+        <ul role="nav">
+          <li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li>
+          <li><NavLink to="/repos">Repos</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          </ul>
+
+          { this.props.children }
+      </div>
+    )
+  }
+})
