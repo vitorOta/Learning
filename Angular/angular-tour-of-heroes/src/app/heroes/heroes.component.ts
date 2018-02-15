18f59c26bd52d1b5585e817f42d105c0363adf0a
vitorota <ota.vitor@gmail.com>
2018-02-15 11:41:42 -0200

    continue tutorial (stop on step 6)

--- Angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../Hero'
+import { Hero } from '../hero'
+import { HeroService } from '../hero.service'
+
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +10,15 @@ import {Hero} from '../Hero'
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes: Hero[];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
+  getHeroes():void{
+    this.heroService.getHeroes().subscribe(heroes=> this.heroes = heroes);
+  }
 }
