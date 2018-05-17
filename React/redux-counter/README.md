b8f32ca6053f48829255cc77f0ab7d2a53145527
vitorota <ota.vitor@gmail.com>
2018-05-17 11:51:57 -0300

    add/finish counter-redux

---
+++ React/redux-counter/README.md
@@ -0,0 +1,4 @@
+
+Tutorial from [https://daveceddia.com/what-does-redux-do/](https://daveceddia.com/what-does-redux-do/) and [https://daveceddia.com/how-does-redux-work/](https://daveceddia.com/how-does-redux-work/)
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ React/redux-counter/src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Counter from "./Counter";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+
+
+const initialState = {
+    count: 0
+};
+
+function reducer(state = initialState, action) {
+
+    switch (action.type) {
+        case "INCREMENT":
+            return {
+                count: state.count + 1
+            };
+        case "DECREMENT":
+            return {
+                count: state.count - 1
+            };
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
+const App = () => (
+    <Provider store={store}>
+        <Counter/>
+    </Provider>
+);
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
