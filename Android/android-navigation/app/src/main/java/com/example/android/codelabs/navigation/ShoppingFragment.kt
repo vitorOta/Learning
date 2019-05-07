200f4678d74f2a81ac6355574dccfe8edd5b9a53
vitorOta <vitorOta>
2019-05-07 19:38:07 -0300

    [android-navigation] step 12 done (finish)

---
+++ Android/android-navigation/app/src/main/java/com/example/android/codelabs/navigation/ShoppingFragment.kt
@@ -0,0 +1,23 @@
+package com.example.android.codelabs.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ *
+ * @author Vitor Ota
+ * @since 07/05/2019
+ */
+class ShoppingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.shopping_fragment, container, false)
+    }
+}
