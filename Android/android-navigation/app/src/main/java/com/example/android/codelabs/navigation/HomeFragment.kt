cdc89adab73cf9bfb13f06976c8c2c6bd0da057b
vitorOta <vitorOta>
2019-05-07 15:16:16 -0300

    [android-navigation] step 5 done

--- Android/android-navigation/app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
+++ Android/android-navigation/app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
@@ -18,16 +18,18 @@ package com.example.android.codelabs.navigation
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 
 /**
  * Fragment used to show how to navigate to another destination
  */
 class HomeFragment : Fragment() {
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         setHasOptionsMenu(true)
         return inflater.inflate(R.layout.home_fragment, container, false)
@@ -36,12 +38,13 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //TODO STEP 5 - Set an OnClickListener, using Navigation.createNavigateOnClickListener()
-//        val button = view.findViewById<Button>(R.id.navigate_destination_button)
-//        button?.setOnClickListener {
-//            findNavController().navigate(R.id.flow_step_one_dest, null)
-//        }
-        //TODO END STEP 5
+        val button = view.findViewById<Button>(R.id.navigate_destination_button)
+        button?.setOnClickListener(
+            Navigation.createNavigateOnClickListener(
+                R.id.flow_step_one_dest,
+                null
+            )
+        )
 
         //TODO STEP 6 - Set NavOptions
 //        val options = navOptions {
